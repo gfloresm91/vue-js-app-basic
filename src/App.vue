b4c147803a98c30a654fad561d3d228d0e873d98
Gabriel Flores <gabrielfloresmonsalve@gmail.com>
2018-12-07 12:07:38 -0300

    Se agrega api de last.fm

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,30 @@
 <template lang="pug">
   #app
     img(src='./assets/logo.png')
-    h1 {{ msg }}
-    h2 Essential Links
+    h1 Bienvenido a Vue music con last.fm api
+    h2 Top artistas en Chile
     ul
-      li
-        a(href='https://vuejs.org', target='_blank') Core Docs
-      li
-        a(href='https://forum.vuejs.org', target='_blank') Forum
-      li
-        a(href='https://chat.vuejs.org', target='_blank') Community Chat
-      li
-        a(href='https://twitter.com/vuejs', target='_blank') Twitter
-    h2 Ecosystem
-    ul
-      li
-        a(href='http://router.vuejs.org/', target='_blank') vue-router
-      li
-        a(href='http://vuex.vuejs.org/', target='_blank') vuex
-      li
-        a(href='http://vue-loader.vuejs.org/', target='_blank') vue-loader
-      li
-        a(href='https://github.com/vuejs/awesome-vue', target='_blank') awesome-vue
+      li(v-for="artista in artistas") {{ artista.name }}
 </template>
 
 <script>
+import getArtists from './api'
+
 export default {
   name: "app",
   data() {
     return {
-      msg: "Welcome to Your Vue.js App"
-    };
+      artistas: []
+    }
+  },
+  mounted: function(){
+    const self = this
+    getArtists()
+      .then(function(artistas){
+        self.artistas = artistas
+      })               
   }
-};
+}
 </script>
 
 <style lang="stylus">
