6b6750468a08571180a2533a01cbaeddd0faaf0d
Gabriel Flores <gabrielfloresmonsalve@gmail.com>
2018-12-07 14:50:32 -0300

    Se agrega selección de artistas por país

--- src/App.vue
+++ src/App.vue
@@ -2,56 +2,88 @@
   <div id="app">
     <img src="./assets/logo.png">
     <h1>Bienvenido a Vue music con last.fm api</h1>
-    <h2>Top artistas en Chile</h2>
+    <select v-model="selectedCountry">
+      <option
+        v-for="country in countries"
+        :key="country.value"
+        :value="country.value"
+      >{{ country.name }}</option>
+    </select>
+    <h2>Top artistas en {{ selectedCountry |capitalize }}</h2>
     <topartistas v-for="artista in artistas" :artista="artista" :key="artista.mbid"></topartistas>
   </div>
 </template>
 
 <script>
-import topartistas from './components/topartistas.vue'
-import getArtists from './api'
+import topartistas from "./components/topartistas.vue";
+import getArtists from "./api";
 
 export default {
   name: "app",
   data() {
     return {
-      artistas: []
-    }
+      artistas: [],
+      countries: [
+        { name: "Chile", value: "chile" },
+        { name: "Argentina", value: "argentina" },
+        { name: "Colombia", value: "colombia" },
+        { name: "España", value: "spain" }
+      ],
+      selectedCountry: "chile"
+    };
   },
-  components:{
+  components: {
     topartistas
   },
-  mounted: function(){
-    const self = this
-    getArtists()
-      .then(function(artistas){
-        self.artistas = artistas
-      })
+  methods: {
+    refreshArtists(){
+      const self = this;
+      getArtists(this.selectedCountry).then(function(artistas) {
+      self.artistas = artistas;
+    })
+    }
+  },
+  mounted(){
+    this.refreshArtists()
+  },
+  watch: {
+    selectedCountry() {
+      this.refreshArtists()
+    }
+  },
+  filters: {
+    capitalize: function(str) {
+      return str.charAt(0).toUpperCase() + str.slice(1);
+    }
   }
-}
+};
 </script>
 
 <style lang="stylus">
-#app
-  font-family "Avenir", Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 
-h1,
-h2
-  font-weight normal
+h1, h2 {
+  font-weight: normal;
+}
 
-ul
-  list-style-type none
-  padding 0
+ul {
+  list-style-type: none;
+  padding: 0;
+}
 
-li
-  display inline-block
-  margin 0 10px
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
 
-a
-  color #42b983
+a {
+  color: #42b983;
+}
 </style>
